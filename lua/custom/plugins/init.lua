a6574fee61d9fc6e134b0677e8395a5108970b17
Ahrar Monsur <ahrar.monsur@onevest.com>
2023-12-04 06:17:17 -0500

    Fixes harpoon keymaps

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,7 +1,12 @@
 return {
   'mbbill/undotree',
+
   'nvim-treesitter/playground',
-  'theprimeagen/harpoon',
+  {
+    'theprimeagen/harpoon',
+    branch = "harpoon2",
+    requires = { {"nvim-lua/plenary.nvim"} }
+  },
   {
     "nvim-telescope/telescope-file-browser.nvim",
     dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
